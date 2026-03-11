1985f95c58390aa74995f53f1e556dacd641fb30
stevenkim18 <11882@naver.com>
2026-03-11 16:30:44 +0900

    docs(api): restore api-docs route and local swagger server (STE-6)

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
               default: 'localhost:3000'
